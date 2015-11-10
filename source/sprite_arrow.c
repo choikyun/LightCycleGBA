a57fddb2e90276e76b361ea0e1488a63156fb9f9
choikyun <kyun34@gmail.com>
2015-11-10 20:43:26 +0900

    choi@motoyama

---
+++ source/sprite_arrow.c
@@ -0,0 +1,24 @@
+
+//{{BLOCK(sprite_arrow)
+
+//======================================================================
+//
+//	sprite_arrow, 8x8@8, 
+//	+ 1 tiles not compressed
+//	Total size: 64 = 64
+//
+//	Time-stamp: 2015-11-09, 22:30:59
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+const unsigned short sprite_arrowTiles[32] __attribute__((aligned(4)))=
+{
+	0xFFFF,0x0000,0x0000,0x0000,0xFFFF,0xFFFF,0x0000,0x0000,
+	0xFFFF,0xFFFF,0xFFFF,0x0000,0xFFFF,0xFFFF,0xFFFF,0x00FF,
+	0xFFFF,0xFFFF,0xFFFF,0x0000,0xFFFF,0xFFFF,0x0000,0x0000,
+	0xFFFF,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,0x0000,
+};
+
+//}}BLOCK(sprite_arrow)
